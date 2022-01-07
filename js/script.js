9f7515c6ff68a5e51d058a44d8e4f66bea1c8d0c
Alexander <kinbean444@gmail.com>
2022-01-07 04:02:32 +0200

    php

--- js/script.js
+++ js/script.js
@@ -221,8 +221,63 @@ document.addEventListener('DOMContentLoaded', () => {
     new Menu("img/tabs/post.jpg", "post", ".menu .container", 'Меню "Постное"', "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.", 15, 'menu__item').render();
 
 
+    // ФОРМА ОТПРАВКИ ДАННЫХ
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо !',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json');
+
+            const formData = new FormData(form);
+
+            const obj = {};
+            formData.forEach(function(value, key) {
+               obj[key] = value; 
+            });
+
+            const json = JSON.stringify(obj);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+                setTimeout(() => {
+                    statusMessage.remove();
+                }, 2000);
+
+            });
+
+            form.reset();
+
+        });
+
+    }
 
 
 });
-
-
